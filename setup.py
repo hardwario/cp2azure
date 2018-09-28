9d28ee0a594e1b3862f26de88fa3312cc01702fc
Pavel Hübner <pavel.hubner@gmail.com>
2018-09-28 13:44:33 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name='cmctl',
+    packages=['cmctl'],
+    version='1.0.0',
+    description='COOPER to Azure IoT Hub',
+    url='https://github.com/hardwario/cp2azure',
+    author='HARDWARIO s.r.o.',
+    author_email='ask@hardwario.com',
+    license='MIT',
+    keywords = ['cooper', 'azure', 'iot'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Scientific/Engineering :: Human Machine Interfaces',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research'
+    ],
+    install_requires=[
+        'azure-iothub-device-client==1.4.3', 'click==6.7', 'PyYAML==3.13','pyzmq==17.1.2'
+    ],
+    entry_points='''
+        [console_scripts]
+        cmctl=cp2azure.cli:main
+    '''
+)
